efd96e9bd74c2e77136425648f69193776abe132
Luke Manyamazi <lukemanyamazi1@gmail.com>
2025-11-08 19:37:55 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Group One Q&A Site</h1>
+      <p>Connecting CYF trainees with answers and collaboration.</p>
+    </div>
+  );
+}
+
+export default App;
